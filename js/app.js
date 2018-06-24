29d89e4afe9178bd2f3b0690b11db5b4d4abff4f
Eli Shay <El_mn123@hotmail.com>
2018-06-24 23:51:46 +0300

    feat: the function cardOpen was updated
    the function is now checking whether a matching cards were found
    (implemented by using a stack data structure)

--- js/app.js
+++ js/app.js
@@ -1,14 +1,49 @@
 
+class Stack
+{
+    constructor()
+    {
+        this.items = [];
+    }
+
+    push(element)
+    {
+        this.items.push(element);
+    }
+
+    peek()
+    {
+        return this.items[this.items.length - 1];
+    }
+
+    pop()
+    {
+        if (this.items.length == 0)
+        {
+            return "Underflow";
+        }
+
+        return this.items.pop();
+    }
+
+    size()
+    {
+        return this.item.length;
+    }
+}
+
 arrangeDeck();
 
 
 
 
 // Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
+function shuffle(array)
+{
     var currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
+    while (currentIndex !== 0)
+    {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
         temporaryValue = array[currentIndex];
@@ -28,26 +63,78 @@ function shuffle(array) {
 *   - loop through each card and create its HTML
 *   - add each card's HTML to the page
 */
-function arrangeDeck() {
+function arrangeDeck()
+{
 
-const cards_array = shuffle([].slice.call(document.body.querySelectorAll('.deck .card')));
+    const cards_array = shuffle([].slice.call(document.body.querySelectorAll('.deck .card')));
 
-const fragment = document.createDocumentFragment();
+    const fragment = document.createDocumentFragment();
 
-for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < 16; i++)
+    {
         cards_array[i].addEventListener('click',cardOpen);
         fragment.appendChild(cards_array[i]);
     }
 
-document.querySelector('.deck').appendChild(fragment);
+    document.querySelector('.deck').appendChild(fragment);
 }
 
 
+var stack = new Stack();
+let counter = 0;
+let card1 = null, card2 = null;
+let ready_to_pick = true;
 
-function cardOpen() {
+function cardOpen()
+{
 
-  console.log('opened');
-  this.className += " open show";
+    console.log('opened');
+
+    // while(ready_to_pick)
+    // {
+        console.log('opened');
+
+        this.className += " open show";
+        counter++;
+
+        if (counter % 2 == 0)
+        {
+            // ready_to_pick = false;
+
+            card1 = stack.peek();
+            card2 = this;
+
+            if (card1.querySelector('i').className === card2.querySelector('i').className)
+            {
+                card1.className = "card match";
+                card1.removeEventListener('click',cardOpen);
+
+                card2.className += "card match";
+                card2.removeEventListener('click',cardOpen);
+
+                stack.push(card2);
+            }
+
+            else
+            {
+                setTimeout(function(){
+                    card1.className = "card";
+                    stack.pop();
+                    card2.className = "card";
+                    console.log("Hello"); }, 900);
+
+            }
+
+            // ready_to_pick = true;
+        }
+
+        else
+        {
+            stack.push(this);
+        }
+
+
+    // }
 }
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -59,3 +146,18 @@ function cardOpen() {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+
+
+
+// stack.push(10);
+// stack.push(20);
+// stack.push(30);
+//
+// console.log(stack.peek());
+//
+// console.log(stack.pop());
+// console.log(stack.peek());
+//
+// delete stack;
